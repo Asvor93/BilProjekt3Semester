76eb93fc89f64e179c5668ad8bd7592dd6e932d9
Thei1186 <44639852+Thei1186@users.noreply.github.com>
2019-11-26 09:17:17 +0100

    Update Car.cs

--- BilProjekt3Semester.Core.Entity/Car.cs
+++ BilProjekt3Semester.Core.Entity/Car.cs
@@ -2,6 +2,9 @@
 {
     public class Car
     {
+        public int CarId { get; set; }
+        public bool Sold { get; set; }
+        public DateTime SoldDate { get; set; }
         public CarAccessories CarAccessories { get; set; }
         public CarDetails CarDetails { get; set; }
         public CarSpecs CarSpecs { get; set; }
