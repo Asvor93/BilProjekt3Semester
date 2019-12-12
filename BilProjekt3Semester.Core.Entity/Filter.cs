783e349c4ed73c0b68fce4f2390708c52c0f4de2
Thei1186 <44639852+Thei1186@users.noreply.github.com>
2019-12-12 11:28:04 +0100

    Update Filter.cs

--- BilProjekt3Semester.Core.Entity/Filter.cs
+++ BilProjekt3Semester.Core.Entity/Filter.cs
@@ -5,5 +5,7 @@
         public int CurrentPage { get; set; }
         public int ItemsPrPage { get; set; }
         public string SearchBrandNameQuery { get; set; }
+        public int minPrice { get; set; }
+        public int maxPrice { get; set; }
     }
 }
